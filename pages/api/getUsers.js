72b4bc39b8926c538839e65b52b6a2d636c3154f
Tong Sheng Ng <tongsheng.ng@cheetahdigital.com>
2020-12-14 21:42:42 +0800

    debug

--- pages/api/getUsers.js
+++ pages/api/getUsers.js
@@ -6,6 +6,7 @@ const handler = nextConnect();
 handler.use(middleware);
 
 handler.get(async (req, res) => {
+  console.log("process.env", process.env)
   const users = await req.db.collection("users").find({}).toArray();
   res.json(users);
 });
