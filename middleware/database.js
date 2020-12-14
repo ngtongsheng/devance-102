9ad21dac9524dd04332e3e213258b03a966916f4
Tong Sheng Ng <tongsheng.ng@cheetahdigital.com>
2020-12-14 21:45:35 +0800

    debug

--- middleware/database.js
+++ middleware/database.js
@@ -1,6 +1,8 @@
 import { MongoClient } from "mongodb";
 import nextConnect from "next-connect";
 
+console.log("middleware", process.env);
+
 const { MONGODB_URI, MONGODB_DB } = process.env;
 
 const client = new MongoClient(MONGODB_URI, {
@@ -9,6 +11,7 @@ const client = new MongoClient(MONGODB_URI, {
 });
 
 async function database(req, res, next) {
+  console.log("client.isConnected()", client.isConnected())
   if (!client.isConnected()) {
     await client.connect();
   }
